f8dd74c0c8a85f8c9454b2b9cd33efc911dfa68e
NuoKuai <yang_seventy_two@qq.com>
2018-08-20 17:11:30 +0800

    加入了主界面跳转商店界面

--- app/src/main/java/com/wy/schooltakenout/BottomNavition/HomeFragment.java
+++ app/src/main/java/com/wy/schooltakenout/BottomNavition/HomeFragment.java
@@ -1,5 +1,8 @@
 package com.wy.schooltakenout.BottomNavition;
 
+import android.content.Intent;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
@@ -10,6 +13,7 @@ import android.view.ViewGroup;
 
 import com.wy.schooltakenout.Adapter.StoreAdapter;
 import com.wy.schooltakenout.Data.Store;
+import com.wy.schooltakenout.HomePage.StoreActivity;
 import com.wy.schooltakenout.R;
 
 import java.util.ArrayList;
@@ -42,19 +46,35 @@ public class HomeFragment extends Fragment {
 
     private void init(View view) {
         storeView = view.findViewById(R.id.store_view);
+
+        //添加一些商店数据用于测试
         storeList = new ArrayList<>();
         List<String> storeTags = new ArrayList<>();
         storeTags.add("不好吃");
         storeTags.add("冷饮");
-
         storeList.clear();
         for(int i=0; i<10; i++) {
-            Store store = new Store("食堂", R.drawable.ic_store_img, storeTags);
+            Store store = new Store("食堂"+i, R.drawable.ic_store_img, storeTags);
             storeList.add(store);
         }
 
-        GridLayoutManager storeLayoutManager=new GridLayoutManager(getContext(),1);
+        //必要，但是不知道有什么用
+        GridLayoutManager storeLayoutManager=new GridLayoutManager(getActivity(),1);
         storeView.setLayoutManager(storeLayoutManager);
-        storeView.setAdapter(new StoreAdapter(storeList));
+        //设置适配器和点击监听
+        StoreAdapter storeAdapter = new StoreAdapter(storeList);
+        storeAdapter.setOnItemClickListener(new StoreAdapter.OnItemClickListener() {
+            @Override
+            public void onClick(View view, int position, Store thisStore) {
+                //进行页面跳转并传递商店数据
+                Intent intent = new Intent(getActivity(), StoreActivity.class);
+                Store store = storeList.get(position);
+                intent.putExtra("name", store.getStoreName());
+                intent.putExtra("img",  store.getStoreImg());
+                intent.putExtra("tags", (ArrayList<String>) store.getStoreTags());
+                startActivity(intent);
+            }
+        });
+        storeView.setAdapter(storeAdapter);
     }
 }
